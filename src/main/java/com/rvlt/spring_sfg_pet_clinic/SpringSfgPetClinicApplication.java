c006b730f30214e48f140c4735750f934bd82409
nx101 <revelight@gmail.com>
2019-01-30 22:33:07 +0200

    Initial Project

---
+++ src/main/java/com/rvlt/spring_sfg_pet_clinic/SpringSfgPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.rvlt.spring_sfg_pet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSfgPetClinicApplication.class, args);
+    }
+
+}
+
